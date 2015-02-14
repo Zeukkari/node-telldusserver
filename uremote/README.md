3a5bd5a88aaf667f201d820cee77a93da290d3a3
Timo Aho <timo.aho@helsinki.fi>
2015-02-14 03:26:45 +0200

    Client for Unified Remote
    For more informatin:
    
    http://www.unifiedremote.com/

---
+++ uremote/README.md
@@ -0,0 +1,6 @@
+Telldus Server client for Unified Remote
+========================================
+	
+For more information:
+	
+	http://www.unifiedremote.com/
---
+++ uremote/remote.lua
@@ -0,0 +1,168 @@
+-----------------------------------------------------
+-- Define your variables here
+-----------------------------------------------------
+
+local tid = -1;
+local counter = 0;
+
+local http = libs.http;
+
+-----------------------------------------------------
+-- Implement your actions here, if needed:
+-----------------------------------------------------
+
+actions.turnOn = function ()
+	layout.message.text = "Let there be light!";
+	local resp = http.get("http://192.168.1.104:8085/aamuvalo");
+end
+
+actions.turnOff = function ()
+	layout.message.text = "Darkness shall rule!";
+	local resp = http.get("http://192.168.1.104:8085/pimeys");
+end
+
+actions.telldus2On = function ()
+	layout.message.text = "Telldus on";
+
+	local headers = { ["Cache-Control"] = "no-cache" };	
+
+	local req = { method = "post",
+	              url = "http://192.168.1.104:8075/telldus/1",
+	              mime = "application/x-www-form-urlencoded",
+	              headers = headers,
+	              content = "cmd=turnOn" };
+
+	local resp = http.request(req);
+end
+
+actions.telldus2Off = function ()
+	layout.message.text = "Telldus off";
+
+	local headers = { key = "value",
+	                  foo = "bar" };	
+
+	local req = { method = "post",
+	              url = "http://192.168.1.104:8075/telldus/1",
+	              mime = "application/x-www-form-urlencoded",
+	              headers = headers,
+	              content = "cmd=turnOff" };
+
+	local resp = http.request(req);
+
+end
+
+
+actions.telldus2On = function ()
+	layout.message.text = "Telldus on";
+
+	local headers = { ["Cache-Control"] = "no-cache" };	
+
+	local req = { method = "post",
+	              url = "http://192.168.1.104:8075/telldus/2",
+	              mime = "application/x-www-form-urlencoded",
+	              headers = headers,
+	              content = "cmd=turnOn" };
+
+	local resp = http.request(req);
+end
+
+actions.telldus2Off = function ()
+	layout.message.text = "Telldus off";
+
+	local headers = { key = "value",
+	                  foo = "bar" };	
+
+	local req = { method = "post",
+	              url = "http://192.168.1.104:8075/telldus/2",
+	              mime = "application/x-www-form-urlencoded",
+	              headers = headers,
+	              content = "cmd=turnOff" };
+
+	local resp = http.request(req);
+
+end
+
+actions.telldus3On = function ()
+	layout.message.text = "Telldus on";
+
+	local headers = { ["Cache-Control"] = "no-cache" };	
+
+	local req = { method = "post",
+	              url = "http://192.168.1.104:8075/telldus/3",
+	              mime = "application/x-www-form-urlencoded",
+	              headers = headers,
+	              content = "cmd=turnOn" };
+
+	local resp = http.request(req);
+end
+
+actions.telldus3Off = function ()
+	layout.message.text = "Telldus off";
+
+	local headers = { key = "value",
+	                  foo = "bar" };	
+
+	local req = { method = "post",
+	              url = "http://192.168.1.104:8075/telldus/3",
+	              mime = "application/x-www-form-urlencoded",
+	              headers = headers,
+	              content = "cmd=turnOff" };
+
+	local resp = http.request(req);
+
+end
+
+actions.lights = function ()
+	layout.message.text = "Telldus on";
+
+	local headers = { ["Cache-Control"] = "no-cache" };	
+
+	local req = { method = "post",
+	              url = "http://192.168.1.104:8075/telldus/4",
+	              mime = "application/x-www-form-urlencoded",
+	              headers = headers,
+	              content = "cmd=turnOn" };
+
+	local resp = http.request(req);
+end
+
+actions.darkness = function ()
+	layout.message.text = "Telldus off";
+
+	local headers = { key = "value",
+	                  foo = "bar" };	
+
+	local req = { method = "post",
+	              url = "http://192.168.1.104:8075/telldus/4",
+	              mime = "application/x-www-form-urlencoded",
+	              headers = headers,
+	              content = "cmd=turnOff" };
+
+	local resp = http.request(req);
+
+end
+
+actions.update = function (progress)
+  print("progress was changed to " .. progress);
+
+  local content;
+
+  if progress == 0 then
+  	content = "cmd=turnOff";
+	else 
+		content = "cmd=dim&dimLevel=" .. progress;
+	end
+
+
+	local headers = { key = "value",
+	                  foo = "bar" };	
+
+	local req = { method = "post",
+	              url = "http://192.168.1.104:8075/telldus/4",
+	              mime = "application/x-www-form-urlencoded",
+	              headers = headers,
+	              content = content };
+
+	local resp = http.request(req);
+
+end
